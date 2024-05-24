9a9cf07e8e1f7fb6eccdf422a5428dc422187e8b
RobertKowalczewski <robert.kow12321@gmail.com>
2024-05-24 14:42:17 +0200

    dasf

--- main.R
+++ main.R
@@ -15,11 +15,13 @@ data = read.csv("games.csv")
 data_separated_genres = data %>%
   separate_rows(Genres, sep = ",")
 
-distinct(data.frame(data_separated_genres$Genres))
 
-counted = count(data_separated_genres, Genres)
+counted = count(data_separated_genres, Genres) %>% filter(n > 100)
 
-data_RPG = data_separated_genres %>% filter(Genres="RPG" | Genres="Action") %>% filter(Peak.CCU>0) %>% filter(Average.playtime.forever>0)
+c(counted$Genres)
+
+
+data_RPG = data_separated_genres %>% filter(Genres=="RPG" | Genres=="Action" | Genres=="Sexual Content" | Genres=="Simulation") %>% filter(Peak.CCU>0) %>% filter(Average.playtime.forever>0)
 #data_RPG$Peak.CCU = (data_RPG$Peak.CCU - mean(data_RPG$Peak.CCU)) / sd(data_RPG$Peak.CCU)
 #data_RPG$Peak.CCU = (data_RPG$Peak.CCU - min(data_RPG$Peak.CCU)) / (max(data_RPG$Peak.CCU) - min(data_RPG$Peak.CCU))
 
@@ -35,17 +37,17 @@ data_RPG$Average.playtime.forever = normalize(data_RPG$Average.playtime.forever)
 
 #ggplot(data_RPG, aes(Genres, Average.playtime.forever)) + geom_violin(fill="white") + geom_jitter(alpha=0.2)  + coord_flip()
 
-ggplot(data_RPG, aes(Genres, Average.playtime.forever)) + geom_violin(fill="white") + geom_dotplot(binaxis="y")  + coord_flip()
+ggplot(data_RPG, aes(Genres, Average.playtime.forever)) + geom_violin(fill="white")  + coord_flip()
 
 
 
-ggplot(data,
- aes(x=Metacritic.score, y=(Positive+1)/(Negative+1))
-) + geom_jitter(aes(alpha=0.4)) + geom_smooth(model=lm) + coord_cartesian(xlim=c(10, 100), ylim=c(0, 150))
+#ggplot(data,
+# aes(x=Metacritic.score, y=(Positive+1)/(Negative+1))
+#) + geom_jitter(aes(alpha=0.4)) + geom_smooth(model=lm) + coord_cartesian(xlim=c(10, 100), ylim=c(0, 150))
 
 
-ggplot(data,
-       aes(x=(Positive+1)/(Negative+1), y=Average.playtime.forever)
-) + geom_jitter(aes(alpha=0.4)) + geom_smooth(model=lm) + coord_trans(x="sqrt")
+#ggplot(data,
+#       aes(x=(Positive+1)/(Negative+1), y=Average.playtime.forever)
+#) + geom_jitter(aes(alpha=0.4)) + geom_smooth(model=lm) + coord_trans(x="sqrt")
 
 
