59067d9ee0e3d3e45be5f306bf24fe4fa8893274
Marcin Leszczyński <venkman2003@gmail.com>
2024-05-24 16:07:19 +0200

    added first vizualization

--- DataVizApp/app.R
+++ DataVizApp/app.R
@@ -1,6 +1,28 @@
 library(shiny)
 library(shinythemes)
+library(dplyr)
+library(tidyr)
 library(ggplot2)
+
+log_transform <- function(x) {
+  return(log1p(x))  # log1p is log(1 + x) to handle zero values
+}
+normalize <- function(x) {
+  return((x - min(x)) / (max(x) - min(x)))
+} 
+
+data = read.csv("../data/games.csv")
+data_separated_genres = data %>%
+  separate_rows(Genres, sep = ",")
+genres = c((count(data_separated_genres, Genres) %>% filter(n > 100))$Genres)
+
+list_genres = list()
+for (g in genres) {
+  if(g!=""){
+    list_genres[[g]] = g
+  }
+
+}
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("united"),
@@ -12,9 +34,9 @@ ui <- fluidPage(theme = shinytheme("united"),
                              sidebarPanel(
                                style = "height: 400px; overflow-y: scroll;",
                                checkboxGroupInput(
-                                 inputId = "genders",
+                                 inputId = "violinGenres",
                                  label = "Genres:",
-                                 choices = list("RPG"="RPG", "Anime"="Anime")
+                                 choices = list_genres
                                )
                              ),
                              mainPanel(
@@ -31,7 +53,7 @@ ui <- fluidPage(theme = shinytheme("united"),
                              sidebarPanel(
                                style = "height: 400px; overflow-y: scroll;",
                                checkboxGroupInput(
-                                 inputId = "genders",
+                                 inputId = "tableGenres",
                                  label = "Genres:",
                                  choices = list("RPG"="RPG", "Anime"="Anime")
                                )
@@ -39,7 +61,7 @@ ui <- fluidPage(theme = shinytheme("united"),
                              mainPanel(
                                wellPanel(
                                  style = "height: 400px; overflow-y: scroll;",
-                                 plotOutput("violinPlots")
+                                 plotOutput("table")
                                )
                              )
                            )
@@ -50,9 +72,12 @@ ui <- fluidPage(theme = shinytheme("united"),
                 )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  output$violinPlots = renderPlot({
+    data_filtered = data_separated_genres %>% filter(Genres %in% input$violinGenres) %>% filter(Peak.CCU>0) %>% filter(Average.playtime.forever>0)
+    ggplot(data_filtered, aes(Genres, Average.playtime.forever)) + geom_violin(fill="white") + geom_jitter(alpha=0.2)  + coord_flip()
+  })
 }
 
 # Run the application 
